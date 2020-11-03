9706544bc9f70dd2cc7e0da0aa2be631d3abd0b0
Gichinsan <dennis.durst@gmail.com>
2020-11-03 14:22:23 +0100

    Add license informations to files

--- src/main/java/de/gichinsan/pwdGenerator/controller/PwdController.java
+++ src/main/java/de/gichinsan/pwdGenerator/controller/PwdController.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of PasswordGenerator.
+ *
+ * PasswordGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * PasswordGenerator is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * PasswordGenerator. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package de.gichinsan.pwdGenerator.controller;
 
 import de.gichinsan.pwdGenerator.core.PasswordGenerator;
