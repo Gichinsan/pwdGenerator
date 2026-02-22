480b1a420bc5567a2cb4b8746c1da763b8d4b97f
Gichinsan <durstdennis@gmail.com>
2026-02-22 11:09:43 +0100

    Update to from 2.3.5 Spring 3.4.5 Update Java 11 tp 21

---
+++ src/main/java/de/gichinsan/pwdGenerator/utils/PasswordGenerator.java
@@ -0,0 +1,76 @@
+/**
+ * This file is part of PasswordGenerator.
+ * <p>
+ * PasswordGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * <p>
+ * PasswordGenerator is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * PasswordGenerator. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package de.gichinsan.pwdGenerator.utils;
+
+public class PasswordGenerator {
+
+    /**
+     *
+     */
+    public PasswordGenerator() {
+    }
+
+
+    /**
+     *
+     * @param slFlag    - smallLetters
+     * @param scFlag    - special characters
+     * @param dFlag     - digits
+     * @param minlenght min length of the password
+     * @return Password as String
+     */
+    public String passwordGenerator(int slFlag, int scFlag, int dFlag,
+                                    int minlenght) {
+        int iNumber;
+        String[] simplePwd = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
+                "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+                "V", "W", "X", "Y", "Z"};
+        String[] smallLetters = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z"};
+        String[] specialCharacters = {"@", "#", "$", "%", "^", "&", "*", "~",
+                "!", "?"};
+        String[] digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        StringBuilder sPWD = new StringBuilder();
+        for (int i = 0; i < minlenght; i++) {
+            iNumber = (int) (Math.random() * ((25 - 1) + 1)) + 1;
+            sPWD.append(simplePwd[iNumber]);
+
+            if (slFlag == 1) {
+
+                iNumber = (int) (Math.random() * ((25 - 1) + 1)) + 1;
+                sPWD.append(smallLetters[iNumber]);
+            }
+
+            if (dFlag == 1) {
+
+                iNumber = (int) (Math.random() * ((8 - 1) + 1)) + 1;
+                sPWD.append(digits[iNumber]);
+            }
+
+            if (scFlag == 1) {
+
+                iNumber = (int) (Math.random() * ((9 - 1) + 1)) + 1;
+                sPWD.append(specialCharacters[iNumber]);
+            }
+        }
+        sPWD = new StringBuilder(sPWD.substring(0, minlenght));
+        return sPWD.toString();
+    }
+}
